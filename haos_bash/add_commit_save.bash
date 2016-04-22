fee20b00afe4470aedf68eca227580a00bfc04c7
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-04-22 19:30:16 +0000

    Add and test script for git remote pushing.

---
+++ haos_bash/add_commit_save.bash
@@ -0,0 +1,29 @@
+function add_commit_push {
+    git add .
+    git commit -m "$1"
+    git push --all
+}
+
+function throw_error {
+    echo "Add a commit message!";
+}
+
+function cache_git {
+    
+    if [ -z "$1" ]
+    then
+        local time_to_store_credentials="300"
+    else
+        local time_to_store_credentials="$1"
+    fi
+    
+    git config --global credential.helper "cache --timeout=$time_to_store_credentials"
+}
+
+if [ -z "$1" ]
+then
+    throw_error
+else
+    cache_git "1800"
+    add_commit_push "$1"
+fi
